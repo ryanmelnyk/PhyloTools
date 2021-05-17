43101e42e8c7c4b4529ddecc110417d1647addc3
Ryan Melnyk <ryan@pivotbio.com>
2021-05-17 09:47:57 -0700

    add argparse to GenbankToProteinFasta.py

--- GenbankToProteinFasta.py
+++ GenbankToProteinFasta.py
@@ -1,21 +1,48 @@
 #!/usr/bin/env python
 
-import sys
+import os
+import argparse
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
-pep = open(sys.argv[2], 'w')
-for seq in SeqIO.parse(open(sys.argv[1], 'r'), 'genbank'):
-    for feat in seq.features:
-        try:
-            if feat.type == "CDS":
-                if feat.qualifiers["translation"][0]:
-                    aa_seq = SeqRecord(Seq(feat.qualifiers["translation"][0]))
-                    aa_seq.id = feat.qualifiers["locus_tag"][0]
-                    aa_seq.description = feat.qualifiers["product"][0]
-                    SeqIO.write(aa_seq, pep, 'fasta')
-        except KeyError:
-            print("Key missing")
 
-pep.close()
+def parse_args():
+    parser = argparse.ArgumentParser(description='''
+Extract protein sequences from a genbank file.
+    ''')
+    parser.add_argument('gbk', type=str, help='Path to Genbank file')
+    parser.add_argument('faa', type=str, help='Path to Output file')
+    parser.add_argument('--locus_tag_field', type=str,
+                        help="alternate to locus_tag qualifier")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    if args.locus_tag_field:
+        tag_field = args.locus_tag_field
+    else:
+        tag_field = "locus_tag"
+
+    pep = open(args.faa, 'w')
+    for seq in SeqIO.parse(open(args.gbk, 'r'), 'genbank'):
+        for feat in seq.features:
+            try:
+                if feat.type == "CDS":
+                    if feat.qualifiers["translation"][0]:
+                        aa_seq = SeqRecord(
+                            Seq(feat.qualifiers["translation"][0])
+                        )
+                        aa_seq.id = feat.qualifiers[tag_field][0]
+                        aa_seq.description = feat.qualifiers["product"][0]
+                        SeqIO.write(aa_seq, pep, 'fasta')
+            except KeyError:
+                pass
+
+    pep.close()
+
+
+if __name__ == '__main__':
+    main()
