02ec8b781fdcacf82872d82dd8ce62fc143735c7
Ryan Melnyk <ryan@pivotbio.com>
2021-10-08 15:04:32 -0700

    change rename.py to use CSV input

--- rename.py
+++ rename.py
@@ -1,34 +1,46 @@
 #!/usr/bin/env python
-#Ryan A. Melnyk
-#schmelnyk@gmail.com
-#UBC Microbiology - Haney Lab
+# Ryan A. Melnyk
+# schmelnyk@gmail.com
+# UBC Microbiology - Haney Lab
 
-import os,argparse
+import os
+import argparse
+
 
 def parse_args():
-	parser = argparse.ArgumentParser(description='''
-Using a tab-separated list of human readable headers to replace tree taxon names.
-	''')
-	parser.add_argument('tree', type=str,help='newick tree file')
-	parser.add_argument('names',type=str,help='tab-separated name list')
-	parser.add_argument('outf',type=str,help='name of new tree file')
-	return parser.parse_args()
+    parser = argparse.ArgumentParser(description='''
+Using a CSV of human readable headers to replace tree taxon names.
+    ''')
+    parser.add_argument('tree',
+                        type=str,
+                        help='newick tree file')
+    parser.add_argument('names',
+                        type=str,
+                        help='comma-separated name list')
+    parser.add_argument('outf',
+                        type=str,
+                        help='name of new tree file')
+    return parser.parse_args()
 
 
 def main():
-	args = parse_args()
+    args = parse_args()
 
-	renamed = {v[0] : v[1] for v in [line.rstrip().split("\t") for line in open(os.path.abspath(args.names),'r').readlines()]}
+    renamed = {
+        v[0]: v[1] for v in
+        [line.rstrip().split(",") for line in
+         open(os.path.abspath(args.names), 'r').readlines()]
+    }
 
-	tree = open(os.path.abspath(args.tree),'r').read()
+    tree = open(os.path.abspath(args.tree), 'r').read()
 
-	for r in renamed:
-		tree = tree.replace(r+":",renamed[r]+":")
+    for r in renamed:
+        tree = tree.replace(r+":", renamed[r]+":")
 
-	o = open(os.path.abspath(args.outf),'w')
-	o.write(tree+"\n")
-	o.close()
+    o = open(os.path.abspath(args.outf), 'w')
+    o.write(tree+"\n")
+    o.close()
 
 
 if __name__ == '__main__':
-	main()
+    main()
